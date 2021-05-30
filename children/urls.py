944e2ec9fd7594791573ef25303450c32f28ed29
Paul-Ngigi <paulkush7777@gmail.com>
2021-05-30 13:53:23 +0300

    ad login and registration

--- children/urls.py
+++ children/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = {
     path('', views.Index, name='index_view'),
+    path('authenticate/login/', views.login, name='login_view'),
+    path('authenticate/register/', views.register, name='signout_view'),
 }
